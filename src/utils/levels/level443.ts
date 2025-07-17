0cbe4329c0ba99bc32cd5c9a056666d5e75eb150
GitHub Action <action@github.com>
2025-07-17 07:19:56 +0000

    Auto-add level 443 for 2025-07-17

---
+++ src/utils/levels/level443.ts
@@ -0,0 +1,41 @@
+import {
+  altoMain,
+  anakiwa,
+  bittersweet,
+  celadon,
+  chardonnay,
+  lightWisteria,
+  nomad,
+  saharaSand,
+  tallow,
+} from "../colors";
+
+const level443 = {
+  size: 10,
+  colorRegions: [
+    ["A", "A", "A", "A", "A", "A", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B"],
+    ["C", "C", "A", "A", "C", "C", "C", "C", "B", "B", "D", "D", "B", "B", "E", "E"],
+    ["F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F"],
+    ["C", "C", "A", "A", "C", "C", "C", "C", "B", "B", "D", "D", "B", "B", "E", "E"],
+    ["C", "C", "C", "C", "C", "C", "C", "C", "B", "B", "D", "D", "D", "D", "E", "E"],
+    ["C", "C", "C", "C", "G", "G", "G", "G", "G", "G", "E", "E", "E", "E", "E", "E"],
+    ["C", "C", "H", "H", "H", "H", "C", "C", "G", "G", "G", "G", "E", "E", "C", "C"],
+    ["C", "C", "H", "H", "H", "H", "C", "C", "C", "C", "I", "I", "I", "I", "C", "C"],
+    ["C", "C", "C", "C", "C", "C", "C", "C", "C", "C", "I", "I", "I", "I", "C", "C"],
+    ["C", "C", "C", "C", "C", "C", "C", "C", "C", "C", "C", "C", "C", "C", "C", "C"],
+  ],
+  regionColors: {
+    A: lightWisteria,
+    B: chardonnay,
+    C: anakiwa,
+    D: celadon,
+    E: altoMain,
+    F: tallow,
+    G: bittersweet,
+    H: saharaSand,
+    I: nomad,
+  },
+  isNew: true,
+};
+
+export default level443;
