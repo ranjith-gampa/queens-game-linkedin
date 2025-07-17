83422b753d6126f4cdaea6101c016f43eb945302
Ranjith Gampa <ranjith.vf473@gmail.com>
2025-07-17 12:46:33 -0700

    feat: Add Level 443

--- src/utils/levels/level443.ts
+++ src/utils/levels/level443.ts
@@ -7,24 +7,6 @@ import {
   lightWisteria,
   nomad,
   saharaSand,
-<<<<<<< HEAD
-  tallow,
-} from "../colors";
-
-const level443 = {
-  size: 10,
-  colorRegions: [
-    ["A", "A", "A", "A", "A", "A", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B"],
-    ["C", "C", "A", "A", "C", "C", "C", "C", "B", "B", "D", "D", "B", "B", "E", "E"],
-    ["F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F"],
-    ["C", "C", "A", "A", "C", "C", "C", "C", "B", "B", "D", "D", "B", "B", "E", "E"],
-    ["C", "C", "C", "C", "C", "C", "C", "C", "B", "B", "D", "D", "D", "D", "E", "E"],
-    ["C", "C", "C", "C", "G", "G", "G", "G", "G", "G", "E", "E", "E", "E", "E", "E"],
-    ["C", "C", "H", "H", "H", "H", "C", "C", "G", "G", "G", "G", "E", "E", "C", "C"],
-    ["C", "C", "H", "H", "H", "H", "C", "C", "C", "C", "I", "I", "I", "I", "C", "C"],
-    ["C", "C", "C", "C", "C", "C", "C", "C", "C", "C", "I", "I", "I", "I", "C", "C"],
-    ["C", "C", "C", "C", "C", "C", "C", "C", "C", "C", "C", "C", "C", "C", "C", "C"],
-=======
 } from "../colors";
 
 const level443 = {
@@ -38,7 +20,6 @@ const level443 = {
     ["C", "G", "G", "C", "C", "H", "H", "C"],
     ["C", "C", "C", "C", "C", "H", "H", "C"],
     ["C", "C", "C", "C", "C", "C", "C", "C"],
->>>>>>> c6ed7c3 (feat: Add Level 443)
   ],
   regionColors: {
     A: lightWisteria,
@@ -46,16 +27,9 @@ const level443 = {
     C: anakiwa,
     D: celadon,
     E: altoMain,
-<<<<<<< HEAD
-    F: tallow,
-    G: bittersweet,
-    H: saharaSand,
-    I: nomad,
-=======
     F: bittersweet,
     G: saharaSand,
     H: nomad,
->>>>>>> c6ed7c3 (feat: Add Level 443)
   },
   isNew: true,
 };
