210bbfd5ee521dda99103917686900694714104c
Ranjith Gampa <ranjith.vf473@gmail.com>
2025-07-17 12:44:13 -0700

    feat: Add Level 443

--- src/utils/levels/level443.ts
+++ src/utils/levels/level443.ts
@@ -7,6 +7,7 @@ import {
   lightWisteria,
   nomad,
   saharaSand,
+<<<<<<< HEAD
   tallow,
 } from "../colors";
 
@@ -23,6 +24,21 @@ const level443 = {
     ["C", "C", "H", "H", "H", "H", "C", "C", "C", "C", "I", "I", "I", "I", "C", "C"],
     ["C", "C", "C", "C", "C", "C", "C", "C", "C", "C", "I", "I", "I", "I", "C", "C"],
     ["C", "C", "C", "C", "C", "C", "C", "C", "C", "C", "C", "C", "C", "C", "C", "C"],
+=======
+} from "../colors";
+
+const level443 = {
+  size: 8,
+  colorRegions: [
+    ["A", "A", "A", "B", "B", "B", "B", "B"],
+    ["C", "A", "C", "C", "B", "D", "B", "E"],
+    ["C", "C", "C", "C", "B", "D", "D", "E"],
+    ["C", "C", "F", "F", "F", "E", "E", "E"],
+    ["C", "G", "G", "C", "F", "F", "E", "C"],
+    ["C", "G", "G", "C", "C", "H", "H", "C"],
+    ["C", "C", "C", "C", "C", "H", "H", "C"],
+    ["C", "C", "C", "C", "C", "C", "C", "C"],
+>>>>>>> c6ed7c3 (feat: Add Level 443)
   ],
   regionColors: {
     A: lightWisteria,
@@ -30,10 +46,16 @@ const level443 = {
     C: anakiwa,
     D: celadon,
     E: altoMain,
+<<<<<<< HEAD
     F: tallow,
     G: bittersweet,
     H: saharaSand,
     I: nomad,
+=======
+    F: bittersweet,
+    G: saharaSand,
+    H: nomad,
+>>>>>>> c6ed7c3 (feat: Add Level 443)
   },
   isNew: true,
 };
