3520178183e0b8a918d3ce77eca149e830679a96
ranjith-gampa <ranjith.vf473@gmail.com>
2025-07-22 06:08:36 -0700

    Fix: Update level448

--- src/utils/levels/level448.ts
+++ src/utils/levels/level448.ts
@@ -7,22 +7,19 @@ import {
   lightWisteria,
   nomad,
   saharaSand,
-  tallow,
 } from "../colors";
 
 const level448 = {
-  size: 10,
+  size: 8,
   colorRegions: [
     ["A", "A", "A", "A", "A", "A", "A", "A"],
     ["A", "A", "A", "A", "A", "B", "A", "A"],
     ["C", "A", "D", "E", "E", "E", "A", "F"],
-    ["G", "G", "G", "G", "G", "G", "G", "G"],
-    ["C", "A", "D", "E", "E", "E", "A", "F"],
-    ["C", "A", "D", "E", "H", "E", "F", "F"],
-    ["C", "A", "D", "E", "H", "E", "F", "F"],
-    ["C", "C", "D", "E", "H", "E", "H", "F"],
-    ["I", "H", "H", "H", "H", "H", "H", "H"],
-    ["I", "H", "H", "H", "H", "H", "H", "H"],
+    ["C", "A", "D", "E", "G", "E", "F", "F"],
+    ["C", "A", "D", "E", "G", "E", "F", "F"],
+    ["C", "C", "D", "E", "G", "E", "G", "F"],
+    ["H", "G", "G", "G", "G", "G", "G", "G"],
+    ["H", "G", "G", "G", "G", "G", "G", "G"],
   ],
   regionColors: {
     A: lightWisteria,
@@ -31,9 +28,8 @@ const level448 = {
     D: celadon,
     E: altoMain,
     F: bittersweet,
-    G: tallow,
-    H: saharaSand,
-    I: nomad,
+    G: saharaSand,
+    H: nomad,
   },
   isNew: true,
 };
