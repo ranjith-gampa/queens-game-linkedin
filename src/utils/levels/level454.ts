17a2bff2becb3513151238659583243e1dd3b349
GitHub Action <action@github.com>
2025-07-28 07:23:18 +0000

    Auto-add level 454 for 2025-07-28

---
+++ src/utils/levels/level454.ts
@@ -0,0 +1,35 @@
+import {
+  altoMain,
+  anakiwa,
+  bittersweet,
+  celadon,
+  chardonnay,
+  lightWisteria,
+  saharaSand,
+} from "../colors";
+
+const level454 = {
+  size: 8,
+  colorRegions: [
+    ["A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "B", "B"],
+    ["A", "A", "C", "C", "D", "D", "D", "D", "D", "D", "A", "A", "B", "B"],
+    ["A", "A", "C", "C", "D", "D", "D", "D", "D", "D", "A", "A", "B", "B"],
+    ["A", "A", "C", "C", "D", "D", "A", "A", "A", "A", "A", "A", "B", "B"],
+    ["A", "A", "C", "C", "C", "C", "E", "E", "B", "B", "B", "B", "B", "B"],
+    ["A", "A", "A", "A", "F", "F", "F", "F", "G", "G", "G", "G", "G", "G"],
+    ["G", "G", "A", "A", "A", "A", "F", "F", "F", "F", "F", "F", "G", "G"],
+    ["G", "G", "G", "G", "G", "G", "G", "G", "G", "G", "G", "G", "G", "G"],
+  ],
+  regionColors: {
+    A: lightWisteria,
+    B: chardonnay,
+    C: anakiwa,
+    D: celadon,
+    E: altoMain,
+    F: bittersweet,
+    G: saharaSand,
+  },
+  isNew: true,
+};
+
+export default level454;
