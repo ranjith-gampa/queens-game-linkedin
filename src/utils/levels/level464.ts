957d52870c48fe79f601c15fd2dbb18c923c654d
GitHub Action <action@github.com>
2025-08-07 07:14:36 +0000

    Auto-add level 464 for 2025-08-07

---
+++ src/utils/levels/level464.ts
@@ -0,0 +1,38 @@
+import {
+  altoMain,
+  anakiwa,
+  bittersweet,
+  celadon,
+  chardonnay,
+  lightWisteria,
+  nomad,
+  saharaSand,
+} from "../colors";
+
+const level464 = {
+  size: 9,
+  colorRegions: [
+    ["A", "A", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B"],
+    ["A", "A", "C", "C", "C", "C", "D", "D", "D", "D", "E", "E", "E", "E", "B", "B"],
+    ["A", "A", "C", "C", "C", "C", "D", "D", "D", "D", "E", "E", "E", "E", "B", "B"],
+    ["A", "A", "C", "C", "D", "D", "D", "D", "D", "D", "D", "D", "E", "E", "B", "B"],
+    ["A", "A", "D", "D", "D", "D", "F", "F", "F", "F", "D", "D", "D", "D", "B", "B"],
+    ["A", "A", "D", "D", "D", "D", "F", "F", "F", "F", "D", "D", "D", "D", "D", "D"],
+    ["A", "A", "G", "G", "D", "D", "D", "D", "D", "D", "D", "D", "H", "H", "A", "A"],
+    ["A", "A", "G", "G", "G", "G", "D", "D", "D", "D", "H", "H", "H", "H", "A", "A"],
+    ["A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A"],
+  ],
+  regionColors: {
+    A: lightWisteria,
+    B: chardonnay,
+    C: anakiwa,
+    D: celadon,
+    E: altoMain,
+    F: bittersweet,
+    G: saharaSand,
+    H: nomad,
+  },
+  isNew: true,
+};
+
+export default level464;
