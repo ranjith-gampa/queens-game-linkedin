fa09a28d737ad990a2fd04a555eaac4ea5a40e7e
GitHub Action <action@github.com>
2025-08-20 07:01:16 +0000

    Auto-add level 477 for 2025-08-20

---
+++ src/utils/levels/level477.ts
@@ -0,0 +1,47 @@
+import {
+  altoMain,
+  anakiwa,
+  bittersweet,
+  celadon,
+  chardonnay,
+  halfBaked,
+  lightOrchid,
+  lightWisteria,
+  nomad,
+  saharaSand,
+  turquoiseBlue,
+} from "../colors";
+
+const level477 = {
+  size: 12,
+  colorRegions: [
+    ["A", "A", "A", "A", "B", "B", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A"],
+    ["A", "A", "B", "B", "B", "B", "B", "B", "C", "C", "A", "A", "A", "A", "A", "A", "D", "D", "D", "D", "A", "A"],
+    ["A", "A", "B", "B", "B", "B", "B", "B", "C", "C", "A", "A", "A", "A", "A", "A", "D", "D", "D", "D", "A", "A"],
+    ["A", "A", "A", "A", "B", "B", "C", "C", "C", "C", "C", "C", "A", "A", "D", "D", "D", "D", "E", "E", "E", "E"],
+    ["A", "A", "A", "A", "A", "A", "A", "A", "C", "C", "A", "A", "A", "A", "D", "D", "E", "E", "E", "E", "A", "A"],
+    ["A", "A", "A", "A", "F", "F", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "E", "E", "A", "A", "A", "A"],
+    ["A", "A", "G", "G", "F", "F", "F", "F", "F", "F", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A"],
+    ["A", "A", "G", "G", "G", "G", "G", "G", "F", "F", "A", "A", "A", "A", "A", "A", "H", "H", "A", "A", "A", "A"],
+    ["A", "A", "A", "A", "A", "A", "G", "G", "A", "A", "A", "A", "A", "A", "I", "I", "H", "H", "A", "A", "A", "A"],
+    ["A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "I", "I", "H", "H", "H", "H", "A", "A"],
+    ["A", "A", "J", "J", "J", "J", "A", "A", "K", "K", "K", "K", "A", "A", "I", "I", "I", "I", "H", "H", "A", "A"],
+    ["J", "J", "J", "J", "J", "J", "K", "K", "K", "K", "K", "K", "A", "A", "A", "A", "I", "I", "A", "A", "A", "A"],
+  ],
+  regionColors: {
+    A: lightWisteria,
+    B: chardonnay,
+    C: anakiwa,
+    D: celadon,
+    E: altoMain,
+    F: bittersweet,
+    G: saharaSand,
+    H: nomad,
+    I: lightOrchid,
+    J: halfBaked,
+    K: turquoiseBlue,
+  },
+  isNew: true,
+};
+
+export default level477;
