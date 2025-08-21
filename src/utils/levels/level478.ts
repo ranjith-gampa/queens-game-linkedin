0a21b57043744340af36b09946e1bc9d2d9cc9ab
GitHub Action <action@github.com>
2025-08-21 07:12:55 +0000

    Auto-add level 478 for 2025-08-21

---
+++ src/utils/levels/level478.ts
@@ -0,0 +1,47 @@
+import {
+  altoMain,
+  anakiwa,
+  bittersweet,
+  celadon,
+  chardonnay,
+  halfBaked,
+  lightOrchid,
+  lightWisteria,
+  nomad,
+  saharaSand,
+  turquoiseBlue,
+} from "../colors";
+
+const level478 = {
+  size: 12,
+  colorRegions: [
+    ["A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A"],
+    ["B", "B", "A", "A", "C", "C", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "D", "D", "A", "A", "A", "A"],
+    ["B", "B", "A", "A", "C", "C", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "D", "D", "A", "A", "A", "A"],
+    ["B", "B", "B", "B", "C", "C", "A", "A", "A", "A", "A", "A", "A", "A", "D", "D", "D", "D", "A", "A", "A", "A"],
+    ["C", "C", "C", "C", "C", "C", "C", "C", "C", "C", "A", "A", "D", "D", "D", "D", "D", "D", "D", "D", "D", "D"],
+    ["E", "E", "E", "E", "C", "C", "A", "A", "A", "A", "A", "A", "A", "A", "D", "D", "D", "D", "F", "F", "F", "F"],
+    ["E", "E", "A", "A", "C", "C", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "D", "D", "F", "F", "F", "F"],
+    ["E", "E", "A", "A", "A", "A", "A", "A", "G", "G", "A", "A", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F"],
+    ["E", "E", "H", "H", "A", "A", "G", "G", "G", "G", "I", "I", "G", "G", "G", "G", "F", "F", "J", "J", "F", "F"],
+    ["H", "H", "H", "H", "H", "H", "G", "G", "I", "I", "I", "I", "I", "I", "G", "G", "J", "J", "J", "J", "J", "J"],
+    ["G", "G", "H", "H", "G", "G", "G", "G", "G", "G", "I", "I", "G", "G", "G", "G", "G", "G", "J", "J", "K", "K"],
+    ["G", "G", "G", "G", "G", "G", "G", "G", "G", "G", "G", "G", "G", "G", "G", "G", "K", "K", "K", "K", "K", "K"],
+  ],
+  regionColors: {
+    A: lightWisteria,
+    B: chardonnay,
+    C: nomad,
+    D: lightOrchid,
+    E: altoMain,
+    F: bittersweet,
+    G: saharaSand,
+    H: anakiwa,
+    I: turquoiseBlue,
+    J: halfBaked,
+    K: celadon,
+  },
+  isNew: true,
+};
+
+export default level478;
