f6925263439ab3ef906bccdc1dead104798ca65d
GitHub Action <action@github.com>
2025-08-24 06:59:17 +0000

    Auto-add level 481 for 2025-08-23

--- src/utils/levels/level480.ts
+++ src/utils/levels/level480.ts
@@ -31,7 +31,6 @@ const level480 = {
     G: saharaSand,
     H: nomad,
   },
-  isNew: true,
 };
 
 export default level480;
---
+++ src/utils/levels/level481.ts
@@ -0,0 +1,47 @@
+import {
+  altoMain,
+  anakiwa,
+  bittersweet,
+  celadon,
+  chardonnay,
+  halfBaked,
+  lightOrchid,
+  lightWisteria,
+  nomad,
+  saharaSand,
+  turquoiseBlue,
+} from "../colors";
+
+const level481 = {
+  size: 12,
+  colorRegions: [
+    ["A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A"],
+    ["A", "A", "A", "A", "A", "A", "B", "B", "B", "B", "C", "C", "A", "A", "A", "A", "D", "D", "D", "D", "A", "A"],
+    ["A", "A", "A", "A", "A", "A", "B", "B", "B", "B", "C", "C", "A", "A", "A", "A", "D", "D", "D", "D", "A", "A"],
+    ["A", "A", "A", "A", "A", "A", "B", "B", "C", "C", "C", "C", "E", "E", "E", "E", "E", "E", "D", "D", "D", "D"],
+    ["A", "A", "A", "A", "A", "A", "B", "B", "B", "B", "C", "C", "E", "E", "D", "D", "E", "E", "E", "E", "D", "D"],
+    ["A", "A", "A", "A", "A", "A", "B", "B", "C", "C", "C", "C", "C", "C", "D", "D", "E", "E", "D", "D", "D", "D"],
+    ["A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "F", "F", "D", "D", "D", "D", "D", "D", "D", "D", "D", "D"],
+    ["F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "G", "G", "G", "G", "H", "H", "I", "I", "D", "D"],
+    ["F", "F", "J", "J", "J", "J", "K", "K", "F", "F", "F", "F", "G", "G", "H", "H", "H", "H", "I", "I", "D", "D"],
+    ["F", "F", "J", "J", "K", "K", "K", "K", "F", "F", "F", "F", "G", "G", "G", "G", "H", "H", "I", "I", "I", "I"],
+    ["F", "F", "J", "J", "J", "J", "K", "K", "F", "F", "F", "F", "G", "G", "H", "H", "H", "H", "H", "H", "F", "F"],
+    ["F", "F", "J", "J", "K", "K", "K", "K", "K", "K", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F"],
+  ],
+  regionColors: {
+    A: lightWisteria,
+    B: chardonnay,
+    C: anakiwa,
+    D: celadon,
+    E: altoMain,
+    F: bittersweet,
+    G: saharaSand,
+    H: nomad,
+    I: lightOrchid,
+    J: halfBaked,
+    K: turquoiseBlue,
+  },
+  isNew: true,
+};
+
+export default level481;
