14bdfad97a4ec126979801126bb33fd825720d96
GitHub Action <action@github.com>
2025-08-28 07:11:53 +0000

    Auto-add level 485 for 2025-08-28

---
+++ src/utils/levels/level485.ts
@@ -0,0 +1,45 @@
+import {
+  altoMain,
+  anakiwa,
+  bittersweet,
+  celadon,
+  chardonnay,
+  halfBaked,
+  lightOrchid,
+  lightWisteria,
+  nomad,
+  saharaSand,
+} from "../colors";
+
+const level485 = {
+  size: 12,
+  colorRegions: [
+    ["A", "A", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "C", "C"],
+    ["A", "A", "D", "D", "B", "B", "B", "B", "B", "B", "B", "B", "E", "E", "E", "E", "F", "F", "C", "C"],
+    ["A", "A", "D", "D", "B", "B", "B", "B", "B", "B", "B", "B", "E", "E", "E", "E", "F", "F", "C", "C"],
+    ["A", "A", "D", "D", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "F", "F", "C", "C"],
+    ["A", "A", "D", "D", "B", "B", "B", "B", "G", "G", "G", "G", "G", "G", "G", "G", "F", "F", "C", "C"],
+    ["A", "A", "D", "D", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "C", "C"],
+    ["A", "A", "D", "D", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "C", "C"],
+    ["A", "A", "D", "D", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "C", "C"],
+    ["A", "A", "B", "B", "H", "H", "H", "H", "H", "H", "H", "H", "H", "H", "H", "H", "B", "B", "C", "C"],
+    ["A", "A", "B", "B", "B", "B", "B", "B", "I", "I", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B"],
+    ["B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B"],
+    ["J", "J", "J", "J", "J", "J", "J", "J", "J", "J", "J", "J", "J", "J", "J", "J", "J", "J", "B", "B"],
+  ],
+  regionColors: {
+    A: lightWisteria,
+    B: altoMain,
+    C: anakiwa,
+    D: celadon,
+    E: nomad,
+    F: chardonnay,
+    G: lightOrchid,
+    H: saharaSand,
+    I: bittersweet,
+    J: halfBaked,
+  },
+  isNew: true,
+};
+
+export default level485;
