b63a5967103c6d69885512e956dcfb0a343b3d97
GitHub Action <action@github.com>
2025-09-06 07:04:02 +0000

    Auto-add level 494 for 2025-09-06

---
+++ src/utils/levels/level494.ts
@@ -0,0 +1,43 @@
+import {
+  altoMain,
+  anakiwa,
+  bittersweet,
+  celadon,
+  chardonnay,
+  halfBaked,
+  lightOrchid,
+  lightWisteria,
+  nomad,
+  saharaSand,
+} from "../colors";
+
+const level494 = {
+  size: 10,
+  colorRegions: [
+    ["A", "A", "A", "B", "B", "B", "B", "C", "C", "C", "C", "C", "C", "C", "C", "C", "C", "C"],
+    ["A", "A", "A", "A", "A", "B", "B", "B", "B", "D", "D", "C", "C", "C", "C", "C", "C", "C"],
+    ["E", "A", "A", "A", "A", "A", "A", "B", "B", "D", "D", "D", "D", "C", "C", "C", "C", "F"],
+    ["E", "E", "E", "A", "A", "A", "A", "A", "A", "D", "D", "C", "C", "C", "C", "F", "F", "F"],
+    ["C", "G", "G", "G", "G", "A", "A", "C", "C", "C", "C", "C", "C", "F", "F", "F", "F", "F"],
+    ["C", "G", "G", "G", "G", "G", "G", "C", "C", "C", "C", "H", "H", "H", "H", "F", "F", "F"],
+    ["C", "C", "C", "G", "G", "C", "C", "C", "C", "H", "H", "H", "H", "H", "H", "F", "F", "F"],
+    ["C", "C", "C", "C", "C", "C", "C", "I", "I", "H", "H", "H", "H", "H", "H", "H", "H", "H"],
+    ["C", "C", "C", "C", "C", "J", "J", "I", "I", "I", "I", "I", "I", "H", "H", "H", "H", "H"],
+    ["C", "C", "C", "J", "J", "J", "J", "J", "J", "I", "I", "I", "I", "H", "H", "H", "H", "H"],
+  ],
+  regionColors: {
+    A: lightWisteria,
+    B: chardonnay,
+    C: anakiwa,
+    D: celadon,
+    E: altoMain,
+    F: bittersweet,
+    G: saharaSand,
+    H: nomad,
+    I: lightOrchid,
+    J: halfBaked,
+  },
+  isNew: true,
+};
+
+export default level494;
