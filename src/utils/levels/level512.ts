d29d7819f41d480f7d8fc33be47bd06b4eecb71b
GitHub Action <action@github.com>
2025-09-24 07:04:31 +0000

    Auto-add level 512 for 2025-09-24

---
+++ src/utils/levels/level512.ts
@@ -0,0 +1,46 @@
+import {
+  altoMain,
+  anakiwa,
+  bittersweet,
+  celadon,
+  chardonnay,
+  halfBaked,
+  lightOrchid,
+  lightWisteria,
+  nomad,
+  saharaSand,
+  turquoiseBlue,
+} from "../colors";
+
+const level512 = {
+  size: 11,
+  colorRegions: [
+    ["A", "A", "A", "A", "B", "B", "B", "B", "B", "B", "C", "C", "C", "C", "D", "D", "D", "D", "D", "D", "D", "D"],
+    ["A", "A", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "C", "C", "C", "C", "C", "C", "D", "D", "D", "D"],
+    ["A", "A", "B", "B", "B", "B", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "C", "C", "C", "C", "D", "D"],
+    ["A", "A", "B", "B", "B", "B", "E", "E", "F", "F", "F", "F", "F", "F", "E", "E", "C", "C", "C", "C", "D", "D"],
+    ["A", "A", "A", "A", "G", "G", "G", "G", "G", "G", "G", "G", "G", "G", "G", "G", "G", "G", "H", "H", "D", "D"],
+    ["A", "A", "A", "A", "G", "G", "G", "G", "G", "G", "G", "G", "G", "G", "G", "G", "G", "G", "H", "H", "D", "D"],
+    ["A", "A", "A", "A", "G", "G", "G", "G", "G", "G", "I", "I", "G", "G", "G", "G", "G", "G", "H", "H", "D", "D"],
+    ["A", "A", "A", "A", "G", "G", "G", "G", "G", "G", "G", "G", "G", "G", "G", "G", "G", "G", "H", "H", "H", "H"],
+    ["A", "A", "A", "A", "J", "J", "G", "G", "G", "G", "G", "G", "G", "G", "G", "G", "H", "H", "H", "H", "H", "H"],
+    ["A", "A", "A", "A", "J", "J", "J", "J", "J", "J", "K", "K", "K", "K", "K", "K", "K", "K", "H", "H", "H", "H"],
+    ["A", "A", "A", "A", "A", "A", "A", "A", "J", "J", "K", "K", "H", "H", "H", "H", "H", "H", "H", "H", "H", "H"],
+  ],
+  regionColors: {
+    A: lightOrchid,
+    B: chardonnay,
+    C: nomad,
+    D: bittersweet,
+    E: altoMain,
+    F: celadon,
+    G: saharaSand,
+    H: anakiwa,
+    I: lightWisteria,
+    J: halfBaked,
+    K: turquoiseBlue,
+  },
+  isNew: true,
+};
+
+export default level512;
