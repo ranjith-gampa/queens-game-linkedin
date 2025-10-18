b562c5086ba617673668eb9f2deaa54127708a12
GitHub Action <action@github.com>
2025-10-18 07:04:11 +0000

    Auto-add level 536 for 2025-10-18

---
+++ src/utils/levels/level536.ts
@@ -0,0 +1,41 @@
+import {
+  altoMain,
+  anakiwa,
+  bittersweet,
+  celadon,
+  chardonnay,
+  lightOrchid,
+  lightWisteria,
+  nomad,
+  saharaSand,
+} from "../colors";
+
+const level536 = {
+  size: 10,
+  colorRegions: [
+    ["A", "A", "B", "B", "B", "B", "B", "B", "B", "B", "C", "C", "C", "C", "C", "C", "C", "C"],
+    ["A", "A", "B", "B", "A", "A", "A", "A", "B", "B", "C", "C", "C", "C", "D", "D", "D", "D"],
+    ["A", "A", "B", "B", "A", "A", "A", "A", "B", "B", "C", "C", "C", "C", "D", "D", "D", "D"],
+    ["A", "A", "A", "A", "A", "A", "A", "A", "C", "C", "C", "C", "C", "C", "E", "E", "D", "D"],
+    ["A", "A", "A", "A", "A", "A", "C", "C", "C", "C", "C", "C", "E", "E", "E", "E", "D", "D"],
+    ["F", "F", "F", "F", "A", "A", "A", "A", "A", "A", "C", "C", "E", "E", "D", "D", "D", "D"],
+    ["F", "F", "A", "A", "A", "A", "A", "A", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E"],
+    ["F", "F", "A", "A", "A", "A", "A", "A", "A", "A", "G", "G", "E", "E", "H", "H", "H", "H"],
+    ["F", "F", "F", "F", "A", "A", "A", "A", "I", "I", "G", "G", "E", "E", "I", "I", "H", "H"],
+    ["A", "A", "A", "A", "A", "A", "A", "A", "I", "I", "I", "I", "I", "I", "I", "I", "H", "H"],
+  ],
+  regionColors: {
+    A: lightWisteria,
+    B: chardonnay,
+    C: anakiwa,
+    D: celadon,
+    E: altoMain,
+    F: bittersweet,
+    G: saharaSand,
+    H: nomad,
+    I: lightOrchid,
+  },
+  isNew: true,
+};
+
+export default level536;
