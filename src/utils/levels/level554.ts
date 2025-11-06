e6433837ec74653d0b2c11f6737055d45d3e6dd2
GitHub Action <action@github.com>
2025-11-06 07:06:19 +0000

    Auto-add level 554 for 2025-11-05

---
+++ src/utils/levels/level554.ts
@@ -0,0 +1,37 @@
+import {
+  altoMain,
+  anakiwa,
+  bittersweet,
+  celadon,
+  chardonnay,
+  lightWisteria,
+  nomad,
+  saharaSand,
+} from "../colors";
+
+const level554 = {
+  size: 8,
+  colorRegions: [
+    ["A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "B", "B", "B", "B", "B", "B"],
+    ["C", "C", "C", "C", "C", "C", "A", "A", "B", "B", "B", "B", "B", "B", "B", "B"],
+    ["C", "C", "C", "C", "D", "D", "D", "D", "B", "B", "B", "B", "B", "B", "B", "B"],
+    ["C", "C", "C", "C", "D", "D", "D", "D", "B", "B", "B", "B", "B", "B", "B", "B"],
+    ["C", "C", "E", "E", "D", "D", "D", "D", "F", "F", "B", "B", "B", "B", "B", "B"],
+    ["G", "G", "E", "E", "D", "D", "E", "E", "F", "F", "B", "B", "F", "F", "B", "B"],
+    ["G", "G", "E", "E", "E", "E", "E", "E", "F", "F", "F", "F", "F", "F", "B", "B"],
+    ["G", "G", "G", "G", "G", "G", "E", "E", "H", "H", "H", "H", "F", "F", "B", "B"],
+  ],
+  regionColors: {
+    A: lightWisteria,
+    B: chardonnay,
+    C: anakiwa,
+    D: celadon,
+    E: altoMain,
+    F: bittersweet,
+    G: saharaSand,
+    H: nomad,
+  },
+  isNew: true,
+};
+
+export default level554;
