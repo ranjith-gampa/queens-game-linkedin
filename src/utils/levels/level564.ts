09fbbcba5acbdcc21a8eb82ff12fc6af2eedd345
GitHub Action <action@github.com>
2025-11-16 07:04:01 +0000

    Auto-add level 564 for 2025-11-15

---
+++ src/utils/levels/level564.ts
@@ -0,0 +1,41 @@
+import {
+  altoMain,
+  anakiwa,
+  bittersweet,
+  celadon,
+  chardonnay,
+  lightWisteria,
+  nomad,
+  saharaSand,
+  tallow,
+} from "../colors";
+
+const level564 = {
+  size: 10,
+  colorRegions: [
+    ["A", "A", "A", "A", "A", "A", "A", "A", "B", "B", "B", "B", "B", "B", "B", "B"],
+    ["A", "A", "C", "C", "A", "A", "A", "A", "B", "B", "B", "B", "D", "D", "B", "B"],
+    ["E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E"],
+    ["A", "A", "C", "C", "A", "A", "A", "A", "B", "B", "B", "B", "D", "D", "B", "B"],
+    ["A", "A", "C", "C", "C", "C", "F", "F", "F", "F", "D", "D", "D", "D", "B", "B"],
+    ["A", "A", "C", "C", "G", "G", "G", "G", "F", "F", "D", "D", "D", "D", "B", "B"],
+    ["A", "A", "C", "C", "G", "G", "H", "H", "F", "F", "F", "F", "D", "D", "B", "B"],
+    ["A", "A", "G", "G", "G", "G", "H", "H", "H", "H", "I", "I", "I", "I", "B", "B"],
+    ["A", "A", "G", "G", "A", "A", "A", "A", "B", "B", "B", "B", "I", "I", "B", "B"],
+    ["A", "A", "A", "A", "A", "A", "A", "A", "B", "B", "B", "B", "B", "B", "B", "B"],
+  ],
+  regionColors: {
+    A: lightWisteria,
+    B: chardonnay,
+    C: anakiwa,
+    D: celadon,
+    E: tallow,
+    F: altoMain,
+    G: bittersweet,
+    H: saharaSand,
+    I: nomad,
+  },
+  isNew: true,
+};
+
+export default level564;
